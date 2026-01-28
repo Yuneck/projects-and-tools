f5fc6d44b5ae5b3f4e5f64940a1b8c055168a817
Jan Garbiec <71603567+Yuneck@users.noreply.github.com>
2026-01-28 19:23:40 +0100

    Refactor test to use synchronous Playwright API

--- kr-radar/tests/test_check_thong.py
+++ kr-radar/tests/test_check_thong.py
@@ -1,35 +1,37 @@
 import pytest
-from playwright.async_api import async_playwright, expect
+from playwright.sync_api import sync_playwright, expect
 
 PRODUCT_URL = "https://korneliarataj.com/en/product/thong-dol-stroju-kapielowego-aqua-2025/"
 
-@pytest.mark.asyncio
-async def test_xl_and_xxl_are_disabled():
-    async with async_playwright() as p:
-        browser = await p.chromium.launch()
-        page = await browser.new_page()
 
-        await page.goto(PRODUCT_URL, timeout=30000)
+def test_xl_and_xxl_are_disabled():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+
+        page.goto(PRODUCT_URL, timeout=30000)
 
         xl = page.locator("li[data-value='xl']")
         xxl = page.locator("li[data-value='xxl']")
         s = page.locator("li[data-value='s']")
+        print("Test started")
         
 
+        if xl.count() == 1:
+            xl_class = xl.get_attribute("class")
+            assert "disabled" in xl_class, f"XL is available!"
+            print("XL not available...")
 
-        # Ensure both exist
-        await expect(xl).to_have_count(1)
-        await expect(xxl).to_have_count(1)
-        await expect(s).to_have_count(1)
+        if xxl.count() == 1:
+            xxl_class = xxl.get_attribute("class")
+            assert "disabled" in xxl_class, f"XXL is available!"
+            print("XXL not available...")
 
+        if s.count() == 1:
+            s_class = s.get_attribute("class")
+            assert "disabled" in s_class, f"S is available!"
+            print("S not available...")
         # Read class attribute
-        xl_class = await xl.get_attribute("class")
-        xxl_class = await xxl.get_attribute("class")
-        s_class = await s.get_attribute("class")
 
-        # Assert "disabled" is present
-        assert "disabled" in xl_class, f"XL is available!"
-        assert "disabled" in xxl_class, f"XXL is available!"
-        #assert "disabled" in s_class, f"S is available!"
 
-        await browser.close()
+        browser.close()
